92642f28c61e3ab5f55c5473cf2de84181e7629a
umatbro <umatbroo@gmail.com>
2018-02-23 22:11:21 +0100

    zad2

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,12 @@
-//Twój kod
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    let userAgent = req.headers['user-agent'];
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+
+    res.end(`Informacja o twojej przeglądarce: ${userAgent}`)
+});
+
+srv.listen(3000, () => {
+    console.log(`Server listening on port 3000`);
+});
