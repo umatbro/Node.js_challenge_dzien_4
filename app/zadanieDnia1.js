56fc50e934ea728687ed2a3f2dab6a1eae00c61e
umatbro <umatbroo@gmail.com>
2018-02-23 22:09:00 +0100

    zad1

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,10 @@
-//Twój kod
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.end('Hello world');
+});
+
+srv.listen(3000, () => {
+    console.log(`Server listening on port 3000`);
+});
